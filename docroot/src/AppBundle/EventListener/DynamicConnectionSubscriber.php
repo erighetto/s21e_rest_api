e116d3b940e6d844dc6f2648bf71267cde49ab6b
Emanuel Righetto <posta@emanuelrighetto.it>
2017-08-06 14:28:01 +0200

    lavoro sul cambio dinamico della connessione a db

---
+++ docroot/src/AppBundle/EventListener/DynamicConnectionSubscriber.php
@@ -0,0 +1,85 @@
+<?php
+
+
+namespace AppBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Doctrine\{
+  ORM\Events, Common\Persistence\Event\LoadClassMetadataEventArgs, DBAL\Driver\Connection, Bundle\DoctrineBundle\Registry
+};
+
+/**
+ * Class DynamicConnectionSubscriber
+ *
+ * @package AppBundle\EventListener
+ */
+class DynamicConnectionSubscriber implements EventSubscriberInterface {
+
+  /**
+   * @var \Doctrine\DBAL\Driver\Connection
+   */
+  private $defaultConnection;
+
+  /**
+   * @var \Doctrine\DBAL\Driver\Connection
+   */
+  private $fosConnection;
+
+  /**
+   * @var \Doctrine\Bundle\DoctrineBundle\Registry
+   */
+  private $doctrine;
+
+  /**
+   * DynamicConnectionSubscriber constructor.
+   *
+   * @param \Doctrine\DBAL\Driver\Connection $defaultConnection
+   * @param \Doctrine\DBAL\Driver\Connection $fosConnection
+   * @param \Doctrine\Bundle\DoctrineBundle\Registry $doctrine
+   */
+  public function __construct(Connection $defaultConnection, Connection $fosConnection, Registry $doctrine)
+  {
+    $this->defaultConnection = $defaultConnection;
+    $this->fosConnection = $fosConnection;
+    $this->doctrine = $doctrine;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public static function getSubscribedEvents() {
+    return array(
+      Events::loadClassMetadata,
+    );
+  }
+
+  /**
+   * @param LoadClassMetadataEventArgs $eventArgs
+   */
+  public function loadClassMetadata(LoadClassMetadataEventArgs $eventArgs)
+  {
+    // the $metadata is the whole mapping info for this class
+    $metadata = $eventArgs->getClassMetadata();
+
+    if (!in_array($metadata->getName(), [
+        'AppBundle\Entity\PosCustomer',
+        'AppBundle\Entity\PosItem'
+      ])) {
+      $this->doctrine->getManager()->flush();
+      if ($this->defaultConnection->isConnected()) {
+        $this->defaultConnection->close();
+      }
+
+      $reflectionConn     = new \ReflectionObject($this->fosConnection);
+      $reflectionParams   = $reflectionConn->getProperty('_params');
+      $reflectionParams->setAccessible(true);
+      $params             = $reflectionParams->getValue($this->fosConnection);
+      $reflectionParams->setValue($this->fosConnection, $params);
+      $reflectionParams->setAccessible(false);
+
+      $this->doctrine->resetManager('default');
+    }
+
+  }
+
+}
