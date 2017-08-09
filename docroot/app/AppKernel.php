f8a338cdf1287f54e3a531ead094118150c7fbda
Emanuel Righetto <posta@emanuelrighetto.it>
2017-08-09 21:51:16 +0200

    ho provato a inserire il paginatore

--- docroot/app/AppKernel.php
+++ docroot/app/AppKernel.php
@@ -3,54 +3,50 @@
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class AppKernel extends Kernel
-{
-    public function registerBundles()
-    {
-        $bundles = [
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new FOS\RestBundle\FOSRestBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle(),
-            new AppBundle\AppBundle(),
-        ];
+class AppKernel extends Kernel {
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
-            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+  public function registerBundles() {
+    $bundles = [
+      new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+      new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+      new Symfony\Bundle\TwigBundle\TwigBundle(),
+      new Symfony\Bundle\MonologBundle\MonologBundle(),
+      new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+      new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+      new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+      new DMR\Bundle\PaginatorBundle\PaginatorBundle(),
+      new FOS\RestBundle\FOSRestBundle(),
+      new JMS\SerializerBundle\JMSSerializerBundle(),
+      new AppBundle\AppBundle(),
+    ];
 
-            if ('dev' === $this->getEnvironment()) {
-                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-                $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
-            }
-        }
+    if (in_array($this->getEnvironment(), ['dev', 'test'], TRUE)) {
+      $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+      $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+      $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
 
-        return $bundles;
+      if ('dev' === $this->getEnvironment()) {
+        $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+      }
     }
 
-    public function getRootDir()
-    {
-        return __DIR__;
-    }
+    return $bundles;
+  }
 
-    public function getCacheDir()
-    {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
-    }
+  public function getRootDir() {
+    return __DIR__;
+  }
 
-    public function getLogDir()
-    {
-        return dirname(__DIR__).'/var/logs';
-    }
+  public function getCacheDir() {
+    return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
+  }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
-    }
+  public function getLogDir() {
+    return dirname(__DIR__) . '/var/logs';
+  }
+
+  public function registerContainerConfiguration(LoaderInterface $loader) {
+    $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
+  }
 }
