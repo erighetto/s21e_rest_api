486ba20eb59c7a355daabb5982af181cbf0628e7
Emanuel Righetto <posta@emanuelrighetto.it>
2019-02-28 00:20:36 +0100

    synfony 4.2

--- .db_update.sh
+++ .db_update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Truncate Tables"
-php httpdocs/bin/console app:db:truncatetables
+php web/bin/console app:db:truncatetables
 echo "Import DB"
-php httpdocs/bin/console doctrine:database:import dumps/dump.sql
+php web/bin/console doctrine:database:import dumps/dump.sql
 echo "Clean up DB"
-php httpdocs/bin/console app:db:cleanup
+php web/bin/console app:db:cleanup
