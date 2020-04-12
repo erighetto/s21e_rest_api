27239d52876890b3a2bb3940a8c7485a96cc048b
Emanuel Righetto <posta@emanuelrighetto.it>
2020-04-12 11:41:35 +0200

    reformat code

--- .db_update.sh
+++ .db_update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Truncate Tables"
-/opt/plesk/php/7.2/bin/php web/bin/console app:db:truncatetables
+/opt/plesk/php/7.3/bin/php web/bin/console app:db:truncatetables
 echo "Import DB"
-/opt/plesk/php/7.2/bin/php web/bin/console doctrine:database:import dumps/dump.sql
+/opt/plesk/php/7.3/bin/php web/bin/console doctrine:database:import dumps/dump.sql
 echo "Clean up DB"
-/opt/plesk/php/7.2/bin/php web/bin/console app:db:cleanup
+/opt/plesk/php/7.3/bin/php web/bin/console app:db:cleanup
