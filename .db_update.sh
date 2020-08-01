d9eb2f6da4e36f3f4568a6c0ffaf0246213eaa3b
Emanuel Righetto <posta@emanuelrighetto.it>
2020-08-01 07:02:42 +0200

    fix path per cron

--- .db_update.sh
+++ .db_update.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+
+HOME="/var/www/vhosts/api.supergabry.it/httpdocs/"
+
 echo "Truncate Tables"
-/opt/plesk/php/7.3/bin/php web/bin/console app:db:truncatetables
+/opt/plesk/php/7.3/bin/php ${HOME}web/bin/console app:db:truncatetables
+
 echo "Import DB"
-/opt/plesk/php/7.3/bin/php web/bin/console doctrine:database:import dumps/dump.sql
+/opt/plesk/php/7.3/bin/php ${HOME}web/bin/console doctrine:database:import dumps/dump.sql
+
 echo "Clean up DB"
-/opt/plesk/php/7.3/bin/php web/bin/console app:db:cleanup
+/opt/plesk/php/7.3/bin/php ${HOME}web/bin/console app:db:cleanup
