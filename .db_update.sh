a692d8ae00a0ac3c46766274469aa1d0f7e9f9f9
Emanuel Righetto <posta@emanuelrighetto.it>
2020-08-01 07:31:33 +0200

    fix path per cron

--- .db_update.sh
+++ .db_update.sh
@@ -6,7 +6,7 @@ echo "Truncate Tables"
 /opt/plesk/php/7.3/bin/php ${HOME}web/bin/console app:db:truncatetables
 
 echo "Import DB"
-/opt/plesk/php/7.3/bin/php ${HOME}web/bin/console doctrine:database:import dumps/dump.sql
+/opt/plesk/php/7.3/bin/php ${HOME}web/bin/console doctrine:database:import ${HOME}dumps/dump.sql
 
 echo "Clean up DB"
 /opt/plesk/php/7.3/bin/php ${HOME}web/bin/console app:db:cleanup
